08106f2dc788f8a153392121090e26c32f288eb7
migueeldr <miguel.diegor@edu.uah.es>
2025-11-03 19:59:19 +0100

    casi hasta el bucle

--- src/Moldeado/Bucle.scala
+++ src/Moldeado/Bucle.scala
@@ -1,12 +1,43 @@
 package Moldeado
 
-import Moldeado.Jugador.liebre
+
 
 def bucleJuego(tablero: TableroJuego, estado: Estado): Jugador=
   TableroClasicoLyS.pintarTablero(estado)
-  if (estado.turno == liebre) then
-    println("Liebre tus movimientos son:"+ MovimientoLiebre.movimientosPosibles(tablero, estado))
+  
+  val movimientos=
+  if (estado.turno == Jugador.liebre) then
+    MovimientoLiebre.movimientosPosibles(tablero, estado)
   else
-    println("Sabueso tus movimientos son:"+ MovimientoSabueso.movimientosPosibles(tablero, estado))
-  val eleccion = scala.io.StdIn.readLine().toInt 
+    MovimientoSabueso.movimientosPosibles(tablero, estado)
+    
+    
+  if (estado.turno == Jugador.liebre) then
+    println("Liebre, tus movimientos son:")
+  else
+    println("Sabueso, tus movimientos son:")
+
+  val movsIndexables = movimientos.toVector  
+  movsIndexables.zipWithIndex.foreach { case (pos, i) => println(s"${i + 1}) $pos") }
+  
+  val eleccion = scala.io.StdIn.readLine().toInt
+  val seleccionIndex = eleccion - 1
+  val destino = movsIndexables(seleccionIndex)
+
+  
+  val nuevoEstado =
+    if (estado.turno == Jugador.liebre) then
+      estado.copy(liebre = destino, turno = Jugador.sabuesos)
+    else
+      val origen = estado.sabuesos.head
+      val nuevosSabuesos = estado.sabuesos - origen + destino
+      estado.copy(sabuesos = nuevosSabuesos, turno = Jugador.liebre)
+  TableroClasicoLyS.esFinPartida(nuevoEstado) match
+    case Some(ganador) =>
+      println("Ha ganado " + ganador)
+      ganador
+    case None =>
+      bucleJuego(tablero, nuevoEstado)
+    
+    
   
